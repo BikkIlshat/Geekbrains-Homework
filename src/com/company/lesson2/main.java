6a0d24f91bb8cddc78d0335222a4f3da1f7e0532
BikkIlshat <HeavyIlshat@rambler.ru>
2021-03-24 21:33:02 +0300

    Сделал 1 задание

--- src/com/company/lesson2/main.java
+++ src/com/company/lesson2/main.java
@@ -1,7 +1,27 @@
 package com.company.lesson2;
 
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
 
+        invertArray ( );
     }
+
+    public static void invertArray ()  {
+        int[] arr = {1, 0, 1, 0, 0, 0, 1};
+        for (int i = 0; i < arr.length; i++ ) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+            System.out.println(i + " " + arr[i]);
+
+        }
+
+    }
+
+
+
 }
