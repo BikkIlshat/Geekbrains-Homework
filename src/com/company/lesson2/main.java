3eb346dd6e04b94997ff8ee7fcf9be5c0e104d9b
BikkIlshat <HeavyIlshat@rambler.ru>
2021-03-25 18:17:20 +0300

    Сделал 3 задание

--- src/com/company/lesson2/main.java
+++ src/com/company/lesson2/main.java
@@ -10,10 +10,13 @@ public class main {
 
         System.out.println("Задание 2");
         fillArray();
+
+        System.out.println("Задание 3");
+        changeArray ();
     }
 
     public static void invertArray ()  {
-        int[] arr = {1, 0, 1, 0, 0, 0, 1};
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr.length; i++ ) {
             if (arr[i] == 1) {
                 arr[i] = 0;
@@ -31,8 +34,15 @@ public class main {
             System.out.println(i + " " + arr[i]);
         }
 
-
-
     }
 
+    public static void changeArray () {
+        int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6);
+            arr[i] = arr[i] * 2;
+            System.out.println(i + " " + arr[i]);
+
+        }
+    }
 }
